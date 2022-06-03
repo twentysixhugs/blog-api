30735c169a22587fdb039894accdb93aaec02748
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-03 20:06:53 +0300

    Set {extended: true} in express.urlencoded

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);

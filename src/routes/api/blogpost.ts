160d953de56829f1ee50b1dbbe19e043031a4a3e
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-04 10:59:47 +0300

    Add routes for comments

--- src/routes/api/blogpost.ts
+++ src/routes/api/blogpost.ts
@@ -1,8 +1,13 @@
 import * as express from 'express';
 import * as blogPostController from '../../controllers/BlogPostController';
+import * as commentController from '../../controllers/CommentController';
 const router = express.Router();
 
+/* Posts */
 router.get('/posts', blogPostController.blogPostGETAll);
+
+/* Single post */
+
 router.get('/posts/:postId', blogPostController.blogPostGET);
 
 router.post('/posts/new', blogPostController.blogPostCreatePOST);
@@ -10,5 +15,20 @@ router.post('/posts/new', blogPostController.blogPostCreatePOST);
 router.put('/posts/:postId', blogPostController.blogPostUpdatePUT);
 
 router.delete('/posts/:postId', blogPostController.blogPostDELETE);
+
+/* Comments */
+
+router.get(
+  '/posts/:postId/comments',
+  commentController.commentAllBlogPostGET,
+);
+router.post(
+  '/posts/:postId/comments/new',
+  commentController.commentCREATE,
+);
+router.delete(
+  '/posts/:postId/comments/:commentId',
+  commentController.commentDELETE,
+);
 
 export default router;

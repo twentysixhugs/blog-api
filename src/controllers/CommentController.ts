13bd35fe9513c86dbf694f9f82b7e03b54610d36
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-04 10:59:43 +0300

    Build Comment controller

---
+++ src/controllers/CommentController.ts
@@ -0,0 +1,95 @@
+import { MiddlewareFn, ResponseError } from '../types';
+import Comment from '../models/Comment';
+import { IUser } from '../models/User';
+import {
+  ValidationChain,
+  body,
+  validationResult,
+} from 'express-validator';
+import passport from '../config/passport';
+import BlogPost from '../models/BlogPost';
+import { HydratedDocument } from 'mongoose';
+
+const commentAllBlogPostGET: MiddlewareFn = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ post: req.params.postId });
+    return res.json({ success: true, comments });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const commentCREATE = (() => {
+  const validationChain: ValidationChain[] = [
+    body('author').trim().escape(),
+    body('text')
+      .trim()
+      .escape()
+      .not()
+      .isEmpty()
+      .withMessage('Comment should contain something, right?'),
+  ];
+
+  const middlewareChain: MiddlewareFn[] = [
+    async (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.json({ success: false, errors });
+      }
+
+      const comment = new Comment({
+        text: req.body.text,
+        author: req.body.author,
+        post: req.params.postId,
+        date: new Date(),
+      });
+      try {
+        await comment.save();
+
+        return res.json({ success: true });
+      } catch (err) {
+        return next(err);
+      }
+    },
+  ];
+
+  return [...validationChain, ...middlewareChain];
+})();
+
+const commentDELETE = (() => {
+  // Comments can be deleted by the post author only
+  const middlewareChain: MiddlewareFn[] = [
+    passport.authenticate('jwt', { session: false }),
+    async (req, res, next) => {
+      try {
+        const post = await BlogPost.findById(req.params.postId).populate(
+          'author',
+        );
+
+        if (!post) {
+          const err: ResponseError = new Error('Post not found');
+          err.status = 404;
+          return next(err);
+        }
+
+        if (post.author.id !== (req.user as HydratedDocument<IUser>).id) {
+          const err: ResponseError = new Error(
+            'You are not the author of the post',
+          );
+          err.status = 401;
+        }
+
+        await Comment.deleteOne({ _id: req.params.commentId });
+
+        res.json({ success: true });
+      } catch (err) {
+        return next(err);
+      }
+    },
+  ];
+
+  return [...middlewareChain];
+})();
+
+export { commentAllBlogPostGET, commentCREATE, commentDELETE };

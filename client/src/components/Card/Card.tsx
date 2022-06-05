e0fd4cfd9ed7582394cad5f2f6f430c406406a17
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-05 11:53:21 +0300

    Add redirect on card click

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 interface ICardProps {
   date: string;
   title: string;
   subtitle: string;
   imgUrl: string;
+  contentUrl: string;
   className?: string;
 }
 
@@ -11,10 +15,20 @@ export default function Card({
   title,
   subtitle,
   imgUrl,
+  contentUrl,
   className,
 }: ICardProps) {
+  const navigate = useNavigate();
+
+  const handleCardOpen: React.MouseEventHandler = (e) => {
+    navigate(contentUrl);
+  };
+
   return (
-    <div className={`c-card ${className ? className : ''}`}>
+    <div
+      className={`c-card ${className ? className : ''}`}
+      onClick={handleCardOpen}
+    >
       <img className="c-card__img" src={imgUrl} alt=""></img>
       <div className="c-card__info">
         <span className="c-card__date">{date}</span>

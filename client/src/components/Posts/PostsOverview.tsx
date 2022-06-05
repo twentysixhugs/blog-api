18570e65789fb222866dbee2bdedee648f5fbe0b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-05 14:19:01 +0300

    Move post splitting logic to PostsOverview

--- client/src/components/Posts/PostsOverview.tsx
+++ client/src/components/Posts/PostsOverview.tsx
@@ -2,34 +2,31 @@ import Card from '../Card';
 import { IPost } from '../../types';
 
 interface IPostsOverviewProps {
-  mainPost: IPost;
-  otherPosts: IPost[];
+  posts: IPost[];
 }
 
-export default function PostsOverview({
-  mainPost,
-  otherPosts,
-}: IPostsOverviewProps) {
+export default function PostsOverview({ posts }: IPostsOverviewProps) {
   return (
     <div className="c-posts-overview">
       <h1 className="c-posts-overview__title">The Blog</h1>
       <Card
-        date={mainPost.datePublishedFormatted!}
-        title={mainPost.title}
+        date={posts[0].datePublishedFormatted!}
+        title={posts[0].title}
         subtitle={'Build subtitle'}
-        imgUrl={mainPost.previewUrl}
-        contentUrl={mainPost.url}
+        imgUrl={posts[0].previewUrl}
+        contentUrl={posts[0].url}
         className="c-card--main"
       ></Card>
-      {otherPosts.map((post) => {
+      {posts.slice(1).map((post) => {
+        console.log(post._id);
         return (
           <Card
-            key={post.id}
+            key={post._id}
             date={post.datePublishedFormatted!}
             title={post.title}
             subtitle={'Build subtitle'}
             imgUrl={post.previewUrl}
-            contentUrl={mainPost.url}
+            contentUrl={post.url}
           ></Card>
         );
       })}

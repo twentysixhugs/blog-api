394fc8e1c9254e628da07d99ca4e0ff38995fe4b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-05 20:21:01 +0300

    Add links to Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header className="c-header">
       <span className="c-header__logo">Self discipline database</span>
+      <Link className="c-header__link" to="/">
+        Home
+      </Link>
+      <Link className="c-header__link" to="/about">
+        About
+      </Link>
     </header>
   );
 }

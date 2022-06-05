041ece5aab2a003e2c51e6a767868d0c34ef1ad0
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-05 20:18:46 +0300

    Build Post

---
+++ client/src/pages/Post/Post.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { formatDate } from '../../components/helpers/formatDate';
+import { IPost, IPostResponse } from '../../types';
+
+export default function Post() {
+  const [currentPost, setCurrentPost] = useState<null | IPost>();
+  const [isError, setIsError] = useState(false);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/api/posts/${postId}`,
+        );
+
+        const data: IPostResponse = await res.json();
+
+        const post: IPost = {
+          ...data.blogPost,
+          url: `/posts/${data.blogPost._id}`,
+          datePublishedFormatted: formatDate(data.blogPost.datePublished),
+        };
+
+        setCurrentPost(post);
+      } catch (err) {
+        setIsError(true);
+      }
+    }
+
+    fetchData().then(() => {
+      setIsError(false);
+    });
+  }, [postId]);
+  return (
+    <>
+      {currentPost && (
+        <div className="c-post">
+          <img
+            className="c-post__image"
+            src={currentPost.previewUrl}
+          ></img>
+          <span className="c-post__author">
+            {currentPost.author.username}
+          </span>
+          <span className="c-post__date">
+            {currentPost.datePublishedFormatted}
+          </span>
+          <h1 className="c-post__title">{currentPost.title}</h1>
+          <p className="c-post__text">{currentPost.text}</p>
+        </div>
+      )}
+    </>
+  );
+}

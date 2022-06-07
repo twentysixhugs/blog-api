d3a07ce48712114264a8be4731b3bb9b7a5107b0
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-07 19:19:45 +0300

    Style loader

--- client/src/components/Loader/Loader.tsx
+++ client/src/components/Loader/Loader.tsx
@@ -1,3 +1,34 @@
+import styled, { keyframes } from 'styled-components';
+
 export default function Loader() {
-  return <div className="c-loader"></div>;
+  return (
+    <Wrapper>
+      <Spinner></Spinner>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  height: calc(100vh - var(--header-offset));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 4px solid #bcbcbc;
+  border-top: 4px solid #ffda9f;
+  border-radius: 100%;
+  animation: ${rotate} 1s linear infinite;
+`;

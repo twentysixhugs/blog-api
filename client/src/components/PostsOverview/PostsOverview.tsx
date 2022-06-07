75be186d745e2a5047861dd357c0a4104e3047f2
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-07 12:05:16 +0300

    Style posts overview

--- client/src/components/PostsOverview/PostsOverview.tsx
+++ client/src/components/PostsOverview/PostsOverview.tsx
@@ -1,5 +1,6 @@
 import Card from '../Card';
 import { IPost } from '../../types';
+import styled from 'styled-components';
 
 interface IPostsOverviewProps {
   posts: IPost[];
@@ -7,19 +8,38 @@ interface IPostsOverviewProps {
 
 export default function PostsOverview({ posts }: IPostsOverviewProps) {
   return (
-    <div className="c-posts-overview">
-      <h1 className="c-posts-overview__title">The Blog</h1>
-      {posts.map((post) => {
-        return (
-          <Card
-            key={post._id}
-            date={post.datePublishedFormatted}
-            title={post.title}
-            subtitle={'Build subtitle'}
-            contentUrl={post.url}
-          ></Card>
-        );
-      })}
-    </div>
+    <Wrapper>
+      <Title>The blog</Title>
+      <PostsWrapper>
+        {posts.map((post) => {
+          return (
+            <Card
+              key={post._id}
+              date={post.datePublishedFormatted}
+              title={post.title}
+              subtitle={'Build subtitle'}
+              contentUrl={post.url}
+            ></Card>
+          );
+        })}
+      </PostsWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 50px 15vw;
+  display: flex;
+  flex-flow: column;
+  gap: 16px;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 800;
+`;
+
+const PostsWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+`;

23715498cb98be315d0207ccd9fc5429f1cc40bb
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-07 19:06:17 +0300

    Style Error

--- client/src/components/Error/Error.tsx
+++ client/src/components/Error/Error.tsx
@@ -1,12 +1,24 @@
+import styled from 'styled-components';
+
 interface IErrorProps {
   message: string;
 }
 
 export default function Error({ message }: IErrorProps) {
   return (
-    <div className="c-error">
-      <h1 className="c-error__title">Something went wrong</h1>
-      <span className="c-error__text">{message}</span>
-    </div>
+    <Wrapper>
+      <h1 className="title">Something went wrong</h1>
+      <span className="text">{message}</span>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - var(--header-offset));
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  color: #4b4b4b;
+`;

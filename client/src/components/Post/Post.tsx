3b60baaa500fc13497e853ecad52e53da3b2401a
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-08 09:05:30 +0300

    Style Post

--- client/src/components/Post/Post.tsx
+++ client/src/components/Post/Post.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 interface IPostProps {
   author: string;
   title: string;
@@ -7,11 +9,36 @@ interface IPostProps {
 
 export default function Post({ author, title, date, text }: IPostProps) {
   return (
-    <div className="c-post">
-      <span className="c-post__author">{author}</span>
-      <span className="c-post__date">{date}</span>
-      <h1 className="c-post__title">{title}</h1>
-      <p className="c-post__text">{text}</p>
-    </div>
+    <StyledPost>
+      <Title>{title}</Title>
+      <DateAuthorWrapper>
+        <span>{date}, by </span>
+        <span>{author}</span>
+      </DateAuthorWrapper>
+      <Text>{text}</Text>
+    </StyledPost>
   );
 }
+
+const StyledPost = styled.div`
+  padding: 50px 20vw;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 800;
+`;
+
+const DateAuthorWrapper = styled.div`
+  margin-top: 1rem;
+  padding-bottom: 0.75rem;
+  border-bottom: 1px solid #e7e7e7;
+  color: #525252;
+`;
+
+const Text = styled.p`
+  margin-top: 1.25rem;
+
+  line-height: 1.8;
+  font-size: 1.3rem;
+`;

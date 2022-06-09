f51df5f60b207e1fd72ac6b6d0f4a5c80c5e64da
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-09 09:47:40 +0300

    Add About route

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import Posts from '../pages/Posts';
 import Post from '../pages/Post';
+import About from '../pages/About';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Posts />}></Route>
           <Route path="posts/:postId" element={<Post />}></Route>
+          <Route path="about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/About/About.tsx
@@ -0,0 +1,9 @@
+import { default as AboutComponent } from '../../components/About';
+
+export default function About() {
+  return (
+    <>
+      <AboutComponent></AboutComponent>
+    </>
+  );
+}

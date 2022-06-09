bd25782fd181a6bbb3936f4739b02e957a02fd55
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-09 10:09:00 +0300

    Style About

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -4,22 +4,40 @@ import Links from './Links';
 export default function About() {
   return (
     <Wrapper>
-      <Title>About</Title>
-      <Description>
-        I made this blog project to practice building REST API and
-        accessing it. It{"'"}s not the first time I work with REST API, but
-        it is the first time I build the API myself. I create the blog
-        posts using CMS, store them on the backend made with Express &
-        MongoDB and fetch from there. You can browse through the posts page
-        by page, open one, leave a comment and see what others think about
-        it.
-      </Description>
-      <Links></Links>
+      <Content>
+        <Title>About</Title>
+        <Description>
+          I made this blog project to practice building REST API and
+          accessing it. It{"'"}s not the first time I work with REST API,
+          but it is the first time I build the API myself. I create the
+          blog posts using CMS, store them on the backend made with Express
+          & MongoDB and fetch from there. You can browse through the posts
+          page by page, open one, leave a comment and see what others think
+          about it.
+        </Description>
+        <Links></Links>
+      </Content>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  height: calc(100vh - var(--header-offset));
+  display: flex;
+  align-items: center;
+  padding: 0 30vw;
+`;
+
+const Content = styled.div`
+  padding: 24px 32px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -12px;
+  border: 1px solid #f1f1f1;
+  border-radius: 12px;
+`;
 
 const Title = styled.h1`
   font-size: 3rem;
@@ -27,7 +45,11 @@ const Title = styled.h1`
 `;
 
 const Description = styled.p`
+  margin-block: 1rem;
+
   line-height: 1.5;
   font-size: 1.2rem;
+  text-align: center;
+
   color: #1d1d1d;
 `;

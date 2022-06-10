59172d117896751c4830dd0ac9c402ff335c4c31
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-10 12:10:11 +0300

    Move CSS reset and global styles to theme

--- client/src/context/Theme/Theme.tsx
+++ client/src/context/Theme/Theme.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { ThemeContext } from './ThemeContext';
+import { ThemeContext, IThemeContext } from './ThemeContext';
 import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export default function Theme({
   children,
@@ -9,5 +10,166 @@ export default function Theme({
 }) {
   const theme = useContext(ThemeContext);
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle theme={theme} />
+      {children}
+    </ThemeProvider>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body,
+  #root {
+    min-height: 100vh;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video,
+  input,
+  button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  
+  margin: 0;
+  font-size: 16px;
+
+  --system-font: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+    'Helvetica Neue', sans-serif;
+  --header-offset: 85px;
+  font-family: 'Nunito', var(--system-font);
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  background: ${({ theme }: { theme: IThemeContext }) =>
+    theme.isDark ? '#181a1b' : 'none'};
+  }
+
+`;
--- client/src/context/Theme/ThemeContext.tsx
+++ client/src/context/Theme/ThemeContext.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 
-interface IThemeContext {
+export interface IThemeContext {
   isDark: boolean;
   toggle: () => void;
 }

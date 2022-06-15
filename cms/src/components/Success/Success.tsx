261037fb8c30391b4a59ce98a5e702584225629a
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-06-15 14:34:01 +0300

    Add Success component

---
+++ cms/src/components/Success/Success.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Navigate, Link } from 'react-router-dom';
+
+interface ISuccessProps {
+  message: string;
+}
+
+export default function Success({ message }: ISuccessProps) {
+  const [secondsLeft, setSecondsLeft] = useState(3);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSecondsLeft((s) => s - 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  if (secondsLeft === 0) {
+    return <Navigate to="/"></Navigate>;
+  } else {
+    return (
+      <Wrapper>
+        <Message>{message}</Message>
+        <Message>
+          Redirect to home page in {secondsLeft}{' '}
+          {secondsLeft > 1 ? 'seconds' : 'second'}
+        </Message>
+        <StyledLink to="/">Go now</StyledLink>
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - var(--header-offset));
+`;
+
+const Message = styled.span`
+  font-size: 1.4rem;
+  color: ${(props) =>
+    props.theme.isDark ? 'var(--text--dark)' : '#202020'};
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1.4rem;
+  color: ${(props) =>
+    props.theme.isDark ? 'var(--text--dark)' : '#202020'};
+  text-decoration: underline;
+`;
